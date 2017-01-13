b18cbb09acabb51600996d496dac45ac8a891068
tdbaddon <tdbaddon@hotmail.com>
2017-01-13 01:25:19 -0500

    22

--- plugin.video.TDBPORNO/_Edit.py
+++ plugin.video.TDBPORNO/_Edit.py
@@ -1,7 +1,7 @@
 import xbmcaddon
 
 MainBase = 'http://www.kgroup.tcomputers.ca/@TDBPORNO/TDBPORNO-HOME.txt'
-addon = xbmcaddon.Addon('plugin.video.TDBTESTER')
+addon = xbmcaddon.Addon('plugin.video.TDBPORNO')
 
 
 
